1ca528c928bc65af45aeed5c38fefdb68ebe719f
Marco <jmdc@my.codergv.com>
2016-11-24 16:26:20 -0600

    Initial commit for employess project

---
+++ employees/client/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>Hello There</div>
+  );
+}
+
+
+
+//wait for meteor to load in the browser
+Meteor.startup(() => {
+  //React render call
+  ReactDOM.render(<App/>, document.querySelector('.container'));
+});
