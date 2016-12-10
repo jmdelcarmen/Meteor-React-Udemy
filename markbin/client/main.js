481c0ea64298ee00b2b8c2ef4c88f9a3cda357d0
Marco <jesusmarcodelcarmen@gmail.com>
2016-12-10 01:34:46 -0600

    connected bin to Db and implemented react-router

--- markbin/client/main.js
+++ markbin/client/main.js
@@ -2,8 +2,22 @@
 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 import App from './components/app';
+import BinsMain from './components/bins/bins_main';
+import BinsList from './components/bins/bins_list';
+import { Bins } from '../imports/collections/bins';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={BinsList} />
+      <Route path="bins/:binId" component={BinsMain} />
+    </Route>
+  </Router>
+);
 
 Meteor.startup(() => {
-  render(<App/>, document.querySelector('.render-target'));
+  render(routes, document.querySelector('.render-target'));
 });
--- markbin/server/main.js
+++ markbin/server/main.js
@@ -4,7 +4,6 @@ import { Meteor } from 'meteor/meteor';
 import { Bins } from '../imports/collections/bins';
 
 Meteor.startup(() => {
-
   Meteor.publish('bins', function () {
     return Bins.find({ownerId: this.userId});
   });
